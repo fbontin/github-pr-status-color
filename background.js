1d6063305963599402d65af71543df55c64aa724
Filip Bontin <filip.bontin@jayway.com>
2021-02-17 21:15:44 +0100

    Make addon work for Chrome

--- background.js
+++ background.js
@@ -1,27 +1,38 @@
+const isFirefox = typeof InstallTrigger !== 'undefined';
+const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+let b;
+if (isFirefox) {
+  b = browser;
+} else if (isChrome) {
+  b = chrome;
+} else {
+  throw Error('No compatible browser found');
+}
+
 function shouldAddCss(url) {
   const { hostname, pathname } = (new URL(url));
-
   const isOnGithub = hostname === "github.com";
   const isPRView = pathname.includes("/pulls");
 
   return isOnGithub && isPRView;
 }
 
-function addCSS(tab) {
+function addCss(tab) {
   if (shouldAddCss(tab.url)) {
     const CSS = `
       a[aria-label*='Review required'] {
-        color: rgb(227, 179, 65) !important;
+        color: rgb(84, 163, 255) !important;
       }
       a[aria-label*='review approval'] {
         color: rgb(86, 211, 100) !important;
       }
       a[aria-label*='requesting changes'] {
-        color: rgb(84, 163, 255) !important;
+        color: rgb(227, 179, 65) !important;
       }
     `;
 
-    browser.tabs.insertCSS(tab.id, { code: CSS });
+    b.tabs.insertCSS(tab.id, { code: CSS });
   }
 }
 
@@ -30,22 +41,27 @@ Initialize the page action: set icon and title, then show.
 Only operates on tabs whose URL's protocol is applicable.
 */
 function initializePageAction(tab) {
-  addCSS(tab);
+  addCss(tab);
 }
 
 /*
 When first loaded, initialize the page action for all tabs.
 */
-var gettingAllTabs = browser.tabs.query({});
-gettingAllTabs.then((tabs) => {
+const addCssOnTabs = (tabs) => {
   for (let tab of tabs) {
-    addCSS(tab);
+    addCss(tab);
   }
-});
+}
+
+if (isFirefox) {
+  b.tabs.query({}).then(addCssOnTabs);
+} else {
+  b.tabs.query({}, addCssOnTabs);
+}
 
 /*
 Each time a tab is updated, reset the page action for that tab.
 */
-browser.tabs.onUpdated.addListener((_id, _changeInfo, tab) => {
+b.tabs.onUpdated.addListener((_id, _changeInfo, tab) => {
   initializePageAction(tab);
 });
